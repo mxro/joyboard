c14ca624fdd7f7cb4afaf11c54bbaf2f6f4c016e
Max Rohde <maxrohde.public@gmail.com>
2019-06-23 11:53:39 +1000

    Uploading initial version

--- apps/storybook/src/index.stories.js
+++ apps/storybook/src/index.stories.js
@@ -5,7 +5,7 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
-import Card from './Card/src';
+import Card from '@joyboard/card';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
